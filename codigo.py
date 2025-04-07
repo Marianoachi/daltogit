5a0726ef8c0b839880f37ef7969af7b38a5eceef
Mariano Achi <achimariano@gmail.com>
2025-04-07 20:37:59 -0300

    askdjf

--- codigo.py
+++ codigo.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 nombre = "pepe"
-=======
-nombre = "Lucas Brando"
->>>>>>> 0b689b5ead3a2331b8d337974a7e41b7f0bf2484
 apellido = "Dalto"
 
 saludo="Hola "+ nombre +" "+ apellido + " como estas?"
